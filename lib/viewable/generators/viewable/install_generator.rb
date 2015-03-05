c462b9a570ffebf27d1fbb40db6e2479239f7209
Tom Cox <tom.jcox@icloud.com>
2015-03-05 00:05:23 +0000

    Rails generator

---
+++ lib/viewable/generators/viewable/install_generator.rb
@@ -0,0 +1,15 @@
+module Viewable
+	module Generators
+		class InstallGenerator < Rails::Generators::Base
+			argument :resource_name, :type => :string, :default => "resource"
+			source_root File.expand_path("../", __FILE__)
+			def install
+				generate "viewable:resource_model #{resource_name}"
+				generate "viewable:booking_model #{resource_name}"
+				generate "viewable:controller #{resource_name}"
+				generate "viewable:views #{resource_name}"
+				generate "viewable:js #{resource_name}"
+				generate "viewable:css #{resource_name}"
+			end
+		end
+	end
