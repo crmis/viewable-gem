0b2787b31fd579e9a21219ad738f305e4c7320a9
Tom Cox <tom.jcox@icloud.com>
2015-03-05 00:07:19 +0000

    Forgot to add viewable to generated paths

--- lib/viewable/generators/viewable/js_generator.rb
+++ lib/viewable/generators/viewable/js_generator.rb
@@ -4,9 +4,9 @@ module viewable
 			source_root File.expand_path('../templates/assets/javascript', __FILE__)
 			argument :viewable_js_name, :type => :string, :default => "resource"
 			def generate_viewable_js
-				template "custom.js", "app/assets/javascripts/custom.js"
-				template "fullcalendar.js", "app/assets/javascripts/fullcalendar.js"
-				template "calendar-editable.js", "app/assets/javascripts/calendar-editable.js"
+				template "custom.js", "app/assets/javascripts/viewable_custom.js"
+				template "fullcalendar.js", "app/assets/javascripts/viewable_fullcalendar.js"
+				template "calendar-editable.js", "app/assets/javascripts/viewable_calendar-editable.js"
 			end
 			private
 			def viewable_name
