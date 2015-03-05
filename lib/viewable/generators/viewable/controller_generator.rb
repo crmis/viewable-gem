4e8ec0ed4b8f235ce4b4e27207cb3a820c0fb151
Tom Cox <tom.jcox@icloud.com>
2015-03-05 00:09:56 +0000

    Custom resource and gem controller generator

---
+++ lib/viewable/generators/viewable/controller_generator.rb
@@ -0,0 +1,21 @@
+module Viewable
+	module Generators
+		class ControllerGenerator < Rails::Generators::Base
+			source_root File.expand_path('../templates/controllers', __FILE__)
+			argument :resource_name, type: :string, default: 'resource'
+			def generate_bookable_controller
+				template "viewing_controller.rb", "app/controllers/viewings_controller.rb"
+				template "resource_controller.rb", "app/controllers/#{resource_name_underscore.pluralize}_controller.rb"
+				route "resources :#{resource_name_underscore.pluralize} do\n" \
+										"\t resources :bookings\n" \
+										"\tend"
+			end
+			private
+			def resource_name_underscore
+				resource_name.underscore
+			end
+			def resource_name_camelize
+				resource_name.camelize
+			end
+		end
+	end
